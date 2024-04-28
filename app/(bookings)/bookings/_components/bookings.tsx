7c213456d675c849273e9622ace2ac6665c726bf
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-04-28 20:51:01 -0300

    feat: validate if confirmed or finished bookings have length to remove from DOM

--- app/(bookings)/bookings/_components/bookings.tsx
+++ app/(bookings)/bookings/_components/bookings.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react"
 import { Prisma } from "@prisma/client"
 import { BookingItem } from "@/app/_components/common/booking-item"
 
@@ -14,28 +15,32 @@ interface BookingsProps {
 
 export const Bookings = async ({ title, isFinished, bookings }: BookingsProps) => {
     return (
-        <section className="pb-6 px-5">
-            <div className={`${isFinished ? "opacity-50": "opacity-100"}`}>
-                <div className="mb-2">
-                    {bookings.length >= 1 && (
-                        <h2 className="text-lg font-normal uppercase text-gray-400">
-                            {title}
-                        </h2>
-                    )}
-                    {!bookings.length && title !=="Finalizados" && (
-                        <h2 className="text-sm">
-                            Nenhum agendamento pendente.
-                        </h2>
-                    )}
-                </div>
-                {bookings.length >= 1 && (        
-                    <div className="flex flex-col gap-3">
-                        {bookings.map((booking) => (
-                            <BookingItem key={booking.id} booking={booking} />
-                        ))}
+        <Fragment>
+            {bookings.length >= 1 && (
+                <section className="pb-6 px-5">
+                    <div className={`${isFinished ? "opacity-50": "opacity-100"}`}>
+                        <div className="mb-2">
+                            {bookings.length >= 1 && (
+                                <h2 className="text-lg font-normal uppercase text-gray-400">
+                                    {title}
+                                </h2>
+                            )}
+                            {!bookings.length && title ==="Confirmados" && (
+                                <h2 className="text-sm">
+                                    Nenhum agendamento pendente.
+                                </h2>
+                            )}
+                        </div>
+                        {bookings.length >= 1 && (        
+                            <div className="flex flex-col gap-3">
+                                {bookings.map((booking) => (
+                                    <BookingItem key={booking.id} booking={booking} />
+                                ))}
+                            </div>
+                        )}
                     </div>
-                )}
-            </div>
-        </section>
+                </section>
+            )}
+        </Fragment>
     )
 }
