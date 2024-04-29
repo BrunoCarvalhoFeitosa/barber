906981bd6e98da8739574390c15c3fc0f127ac2e
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-04-29 00:33:23 -0300

    feat: display total searched results to barbershops page

--- app/(barbershop)/barbershops/_components/barbershop-searched-title.tsx
+++ app/(barbershop)/barbershops/_components/barbershop-searched-title.tsx
@@ -1,15 +1,20 @@
 "use client"
+import { Barbershop } from "@prisma/client"
 
 interface BarbershopTitleProps {
     searchParams?: string | undefined
+    barbershops: Barbershop[]
 }
 
-export const BarbershopSearchedTitle = ({ searchParams }: BarbershopTitleProps) => {
+export const BarbershopSearchedTitle = ({ searchParams, barbershops }: BarbershopTitleProps) => {
     return (
         <div className="py-6 px-5 mb-2">
             <h1 className="text-xl font-bold">
                 Resultados para &quot;{searchParams}&quot;
             </h1>
+            <p className="text-sm text-gray-400">
+                {barbershops.length} resultados encontrados.
+            </p>
         </div>
     )
 }
