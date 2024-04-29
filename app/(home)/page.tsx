5ba6f2916cd831ad5754a5067078a144cbe3ea34
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-04-29 00:51:43 -0300

    feat: change pathname of barbershops to common

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,8 @@ import { getServerSession } from "next-auth"
 import { db } from "@/app/_lib/prisma"
 import { authOptions } from "@/app/_lib/auth"
 import { Header } from "@/app/_components/common/header"
+import { Barbershops } from "@/app/_components/common/barbershops"
 import { Search } from "./_components/search"
-import { Barbershops } from "./_components/barbershops"
 import { Bookings } from "./_components/bookings"
 import { WelcomeMessage } from "./_components/welcome-message"
 
@@ -40,7 +40,7 @@ const HomePage = async () => {
                 <WelcomeMessage />
                 <Search />
                 <Bookings bookings={confirmedBookings} />
-                <Barbershops title="Recomendados" barbershops={barbershops} />
+                <Barbershops title="Barbearias" barbershops={barbershops} />
                 <Barbershops title="Populares" barbershops={recommendedBarbershops} />
             </main>
         </div>
